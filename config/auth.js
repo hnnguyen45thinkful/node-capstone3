d0df397cc63152417403cb4dc130b8bc75489c96
Hieu Nguyen <hnnguyen45@gmail.com>
2016-12-21 23:06:01 -0800

    add facebook auth and delete app-config.js

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '228241340953699',
+		'clientSecret' : '8c190484de3ccb7111d10a9189aa69af',
+ 		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+ 		'profileURL': 'https://graph.facebook.com/v2.8/me?fields=first_name,last_name,email'
+
+	}
+}
